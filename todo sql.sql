27d39f9ad41541d563058d3b02238d453ba0fb78
Jahnavi-Kasaraneni <kasaranenijahnavi@gmail.com>
2025-05-31 10:42:24 +0530

    Add files via upload

---
+++ todo sql.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE spring_todo;
+SHOW DATABASES;
+use spring_todo;
+SHOW tables;
+desc to_do;
+Select * from to_do ;
+select * from to_do WHERE id=1;
+UPDATE to_do SET status=false WHERE id=3;
